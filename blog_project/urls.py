b98a0c365af9c6636b95ea9f01dcc9adb176a1dc
RafaelSchneider91 <rafaelrafa91@hotmail.com>
2022-11-16 22:24:29 -0300

    include summernote in adminpage

--- blog_project/urls.py
+++ blog_project/urls.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from blog.views import index, post_detail  # <------ NEW IMPORT
 
 # import for display image
@@ -9,6 +9,7 @@ from django.conf.urls.static import static  # <------ NEW
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index, name='index'),
+    path('summernote/', include('django_summernote.urls')),
     path('post-detail/<int:id>/', post_detail, name='postDetail')  # <------ NEW
 
 ]
